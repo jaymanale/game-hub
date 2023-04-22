6fb37204c91c23e6d8a10a7ab2044a72d7d2cd5a
jaymanale <jaymanale29@gmail.com>
2023-04-22 15:30:26 +0530

    Clean up game card | UI Improvement

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -16,13 +16,13 @@ const GameCard = ({ game }: Props) => {
         src={getCroppedImageUrl(game.background_image)}
       />
       <CardBody>
-        <Heading fontSize="lg">{game.name}</Heading>
         <HStack justifyContent="space-between">
           <PlatformIconList
             platform={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="lg">{game.name}</Heading>
       </CardBody>
     </Card>
   );
