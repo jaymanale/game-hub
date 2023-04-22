96cf130b2e88798a45eae67f1026f178d0e626c4
jaymanale <jaymanale29@gmail.com>
2023-04-22 16:21:31 +0530

    Removed unused variables

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,5 @@
 import PlatformData from "../data/PlatformData";
 
-interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
-
 // const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
 
 // As Platform list does not change we can render as static content
