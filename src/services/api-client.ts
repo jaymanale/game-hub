c75518e25597547bc8f2a2ac52e637b6479e6056
jaymanale <jaymanale29@gmail.com>
2023-04-27 21:36:43 +0530

    Minor Improvement

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: process.env.RAWG_API_KEY,
+    key: "ef06a685167c46a2b993917e8186a31d",
   },
 });
