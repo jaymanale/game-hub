2a76c329a3de61fea239e85be0abf3475d3e08b5
jaymanale <jaymanale29@gmail.com>
2023-04-27 21:33:23 +0530

    hiding API Key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "ef06a685167c46a2b993917e8186a31d",
+    key: process.env.RAWG_API_KEY,
   },
 });
